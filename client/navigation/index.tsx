8fd8b0ae7cc6fd4407a7064c057d3989d2db84b5
longhainguyen <21020349@vnu.edu.vn>
2024-03-20 22:32:13 +0700

    login

--- client/navigation/index.tsx
+++ client/navigation/index.tsx
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
-import Login from '../app/login/Login';
+import Login from '../app/authen/Login';
 import { COLORS } from '../constants';
 import Home from '../app/home/Home';
 
