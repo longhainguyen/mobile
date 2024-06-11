f656824d12f47865d03b174ca55eebda65f09f5c
QuyManh <quymanhtran453@gmail.com>
2024-06-11 16:45:30 +0700

    Refactor PostParseJsonPipe to handle deleted value in a safer way

--- server/src/pipes/update-post.pipe.ts
+++ server/src/pipes/update-post.pipe.ts
@@ -5,7 +5,7 @@ export class PostParseJsonPipe implements PipeTransform {
     transform(value: any, metadata: ArgumentMetadata) {
         console.log(value);
         if (metadata.type === 'body' && metadata.data === 'deleted') {
-            value = JSON.parse(value);
+            if (value) value = JSON.parse(value);
         }
         return value;
     }
